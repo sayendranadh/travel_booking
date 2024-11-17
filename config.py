8293eb6132b5974d24125175a51bd7fd862d5fb1
20062005nadh <121669585+20062005nadh@users.noreply.github.com>
2024-11-17 15:44:08 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,13 @@
+class Config:
+    MYSQL_HOST = '127.0.0.1'  # Localhost address
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'sai11'
+    MYSQL_DB = 'travel_booking'
+    MYSQL_PORT = 3306  # Default MySQL port
+    SECRET_KEY = 'your_secret_key'  # Replace with your actual secret key
+    MAIL_SERVER = 'smtp.gmail.com'  # Example for Gmail
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'traveldbms@gmail.com'
+    MAIL_PASSWORD = 'Narayana123'
+    

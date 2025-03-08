5e80a02f5910c8f261fce34f2def68b3ef5739c3
sayendranadh <118617049+sayendranadh@users.noreply.github.com>
2025-03-08 16:50:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,10 @@ class Config:
     MYSQL_DB = 'travel_booking'
     MYSQL_PORT = 3306  # Default MySQL port
     SECRET_KEY = 'your_secret_key'  # Replace with your actual secret key
-    MAIL_SERVER = 'smtp.gmail.com'  # Example for Gmail
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = 'traveldbms@gmail.com'
-    MAIL_PASSWORD = 'Narayana123'
+    
+    # SMTP configuration
+    SMTP_SERVER = 'smtp.gmail.com'
+    SMTP_PORT = 587
+    SMTP_USERNAME = 'sayendranadh2005@gmail.com'
+    SMTP_PASSWORD = 'okglqdebivnaataq'
     
